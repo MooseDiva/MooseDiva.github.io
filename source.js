d209f1057f6d0fcffbd555d9c4a4418d2989ba29
MooseDiva <gracep241@gmail.com>
2016-07-06 14:45:51 -0500

    Create source.js

---
+++ source.js
@@ -0,0 +1,47 @@
+var answer = prompt("Welcome to Mairo, a text-based game. Would you like to play?").toLowerCase();
+var turns = 0;
+ if(answer === 'yes') {
+     console.log('Turns = ' + turns);
+     console.log("Type 'help' for a list of commands.");
+     console.log("");
+     console.log("You wake up in a abnormally huge pile of leaves. A new marking rises on your arm, spelling 'Mairo'. You have never heard of anyone named Mairo, and where you are anyway. You decide if your going to get answers, you best look around.");
+     answer = prompt("Command").toLowerCase();
+     console.log("");
+     console.log("> " + answer + " <");
+}
+     
+     switch(answer) { //this switch is the *almost* ENTIRE game //
+         case 'help':
+             turns = 1;
+             console.log("");
+             console.log("'Mairo' is a text-based game using prompt to use commands. List of commands:");
+             console.log("");
+             console.log("Left, right, up, down || Used to move about the world.");
+             console.log("");
+             answer = prompt("Command").toLowerCase();
+             console.log("> " + answer + " <");
+             break;
+        case 'left':
+            turns = 1;
+            console.log('Turns = ' + turns);
+            console.log("");
+            console.log("There is nothing but trees. FOREST.");
+            answer = prompt("Command").toLowerCase();
+     console.log("> " + answer + " <");
+            break;
+        case 'right':
+            turns = 1;
+            console.log('Turns = ' + turns);
+            console.log("");
+            console.log("You are in a clearing with a lockbox, labeled 'Time Capsule', and a forest to your right.");
+            answer = prompt("Command").toLowerCase()
+        default:
+        turns = 1;
+        console.log('Turns = ' + turns);
+        console.log("I don't know that command. Type 'help' for a list of commands.");
+        console.log("");
+        answer = prompt("Command").toLowerCase();
+     console.log("> " + answer + " <");
+        break;
+
+} 
